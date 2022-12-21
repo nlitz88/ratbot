cbf58ef2b7e6309ca9a1a1d861a7c9db26fb063a
Ryan Litzinger <ryan.litz04@gmail.com>
2022-12-21 18:15:00 -0500

    added music cog py and class but not done coding the class yet

---
+++ music_cog.py
@@ -0,0 +1,11 @@
+import discord
+from discord.ext import commands
+
+from youtube_dl import YoutubeDL
+
+class music_cog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+        self.is_playing = False
+        self.is_puased = False
