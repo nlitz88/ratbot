fd3ef4417c4d5129f17458ce4187e1d989aa78f6
Ryan Litzinger <ryan.litz04@gmail.com>
2022-12-21 20:52:37 -0500

    got it to run and fixed the intents issue

--- ratbot.py
+++ ratbot.py
@@ -1,3 +1,4 @@
+import asyncio
 import discord
 from discord.ext import commands
 import os
@@ -6,15 +7,19 @@ import os
 from help_cog import help_cog
 from music_cog import music_cog
 # Create the bot instance and configure it.
-ratbot = commands.Bot(intents=discord.Intents.default(), command_prefix="*")
-
+ratbot = commands.Bot(intents=discord.Intents.all(), command_prefix="*")
 #removes default help commands
 ratbot.remove_command("help")
+
 # Run ratbot with the token provided as an environment variable.
 
-#
-ratbot.add_cog(music_cog(ratbot))
-ratbot.add_cog(help_cog(ratbot))
 
-#
+async def ratbot_setup(bot):
+    """This sets up the music cog and the help cog"""
+    await bot.add_cog(music_cog(bot))
+    await bot.add_cog(help_cog(bot))
+    
+
+asyncio.run(ratbot_setup(ratbot))
+
 ratbot.run(os.getenv("TOKEN"))
